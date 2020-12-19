0beb188088ea12c3d6956045cf6dec906702c839
Ubuntu <ubuntu@ip-172-31-86-189.ec2.internal>
2020-12-19 14:30:15 +0000

    tested

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,11 @@
 ## cd torch_server
 sudo apt-get -y update
 sudo apt-get install -y python3-pip
-pip3 install -y virtualenv
+pip3 install virtualenv
 python3 -m virtualenv venv
 source ./venv/bin/activate
 
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 ##  Setup AWS CLI with Credentials. Next 3 lines are what you paste into ~/.aws/credentials
 mkdir ~/.aws && touch ~/.aws/credentials
--- test_hit.py
+++ test_hit.py
@@ -1,9 +1,9 @@
 import requests
 import json
 
-data = {"bucket": "deploy-ml-api-inferenceapp-1ejhpq7o2563r", "file_key": "torch_EC2/IMG_3770_FRAME_56.png"}
-response = requests.post('http://ec2-3-138-156-190.us-east-2.compute.amazonaws.com:5000/api/v1/pred', 
-	json=data)
-
-print("status_code:", response.status_code)
-print(json.loads(response.content))
+for i in range(343):
+    file_name = "IMG_3736/IMG_3736_FRAME_{}.png".format(i+1)
+    data = {"bucket": "w-yrs-input-images", "file_key": file_name}
+    response = requests.post('http://localhost:5000/api/v1/pred',json=data)
+    print("status_code:", response.status_code)
+    print("Filename: {}. Respose:{}".format(file_name,json.loads(response.content)))
